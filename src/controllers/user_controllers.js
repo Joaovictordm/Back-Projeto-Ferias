11e407b3cecafc8b3a89c7644440c904e3b379f8
João V. Damasceno <joaovictordamascenorodrigues3@gmail.com>
2025-07-18 00:55:03 -0300

    Rotas delete e editar do usuário

--- src/controllers/user_controllers.js
+++ src/controllers/user_controllers.js
@@ -1,6 +1,8 @@
 import {createUser} from "../models/user_models.js";
 import {getUserById} from "../models/user_models.js";
 import { createDataUser } from "../models/user_models.js";
+import { editDataUser } from "../models/user_models.js";
+import { deleteUser } from "../models/user_models.js";
 
 
 //Controler da query que cria o usuário
@@ -30,15 +32,41 @@ export async function createDataUserController(req, res){
         res.status(500).json({mensagem: "Erro ao criar usuário."});
     }
 }
+
 //Controler da query que mostra o usuário por id
 export async function getUserByIdController(req, res){
     try{
         const id = req.params.id;
         const user = await getUserById(id);
-        res.status(200).json(user);
+        res.status(200).json(user)
         return user
     } catch(error){
         console.error("Deu ruim: ", error.message);
         res.status(404).json({mensagem: "O usuário não existe."})
+    }
+}
+
+//Controler da query que atualiza uma informação
+export async function editDataUserController(req, res){
+    try{
+        const id = req.params.id;
+        const data = req.body;
+        const editUser = await editDataUser(data,id);
+        res.status(200).json({mensagem: "Alteração feita com sucesso!"})
+    }catch(error){
+        console.error("Deu ruim, mano: ", error.message);
+        res.status(400).json({mensagem: "Deu algum erro, mano."})
+    }
+}
+
+//Controller pra deletar o usuário
+export async function deleteUserController(req, res){
+    try{
+        const id_login = req.params.id;
+        const deletar = await deleteUser(id_login);
+        res.status(200).json({mensagem: "Usuário deletado com sucesso!"})
+    }catch(error){
+        console.error("Deu ruim, maninho: ", error.message);
+        res.status(500).json({mensagem: "Não deu pra deletar"})
     }
 }
