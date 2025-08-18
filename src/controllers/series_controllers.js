134f9471744bbc18db81d7c04f631c91499507c9
João V. Damasceno <joaovictordamascenorodrigues3@gmail.com>
2025-08-18 17:06:45 -0300

    validação  para aceitar apenas peso e repetição no rota da serie

--- src/controllers/series_controllers.js
+++ src/controllers/series_controllers.js
@@ -1,15 +1,29 @@
 import { createSerie } from "../models/series_models.js";
 import { previousSerie } from "../models/series_models.js";
 import { delSerie } from "../models/series_models.js";
+import { exerciseId } from "../models/exercises_models.js";
 
 //Create a series
 export async function createSerieController(req, res){
     try{
         const id = req.params.id;
-        const {weight, reps} = req.body;
+        const data = req.body;
+        const check = await exerciseId(id);
+        
+        
+        if(!check){
+           return res.status(400).json({message: "exercise does not exist "})
+        }
 
-        const create = await createSerie({id, weight, reps});
-        res.status(200).json({message: "series added", id: create});
+        delete data.id
+        delete data.exercise_id;
+
+        if(data.weight && data.reps){
+            const create = await createSerie({id, weight: data.weight, reps: data.reps});
+            res.status(200).json({message: "series added", id: create});
+
+        }
+
     }catch(error){
         console.error(error.message);
         res.status(400).json({message: "Error adding series"})
--- src/models/routine_models.js
+++ src/models/routine_models.js
@@ -44,7 +44,7 @@ export async function deleteRoutine({id}){
 export async function getExerciseById({id}){
     try{//Aqui é um pouco complicado de entender a query. Ele vai pegar o nome da rotina e o nome do exercicio do user_routine e do exercise. Ele vai pegar apenas as colunas que o user_routine.id é igual ao exercise.routine_id. Ele vai selecionar a rotina com base no id informado.
         const [getById] = await connection.query("SELECT user_routine.routine_name, exercise.exercise_name FROM user_routine JOIN exercise ON user_routine.id = exercise.routine_id WHERE user_routine.id = ?", [id]);
-        return getById;
+        return getById || null;
     }catch(error){
         console.error(error.message);
         throw error;
